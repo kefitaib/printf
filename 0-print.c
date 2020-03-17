c7606dc4b38d50d94e6635c35e89f9ebcf8de613
kefitaib <taibkefi@yahoo.fr>
2020-03-17 06:05:59 -0700

    please

--- 0-print.c
+++ 0-print.c
@@ -28,8 +28,7 @@ int _printf(const char *format, ...)
 	if (!format)
 		return (-1);
 	va_start(list, format);
-	if (list == NULL)
-		return (-1);
+
 	buffer = malloc(1024);
 	if (!buffer)
 		return (-1);
